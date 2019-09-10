45215fbbc69ea1f8aa16113afc5c3a97b89c9739
magic8baller <lturis@gmail.com>
2019-09-10 20:43:51 +0300

    create styled (hero) component to replace Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import {Link} from "gatsby"
 import React from "react"
 import Banner from "../components/Banner"
-import Hero from "../components/Hero"
 import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
 // import About from "../components/About"
 export default () => (
 	<Layout>
-		<Hero>
+		<StyledHero>
 			<Banner title='joseph kalogerakis' info='Painter and Digital Artist'>
 				<Link to='/gallery' className='btn-white'>portfolio</Link>
 			</Banner>
-		</Hero>
+		</StyledHero>
 	</Layout>
 )

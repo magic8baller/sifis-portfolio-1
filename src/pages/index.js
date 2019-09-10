a994542125d478d05810497e3fb387c0c978d58e
magic8baller <lturis@gmail.com>
2019-09-10 20:51:39 +0300

    completed styled hero with gatsby background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
-import {Link} from "gatsby"
+import {graphql, Link} from "gatsby"
 import React from "react"
 import Banner from "../components/Banner"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 // import About from "../components/About"
-export default () => (
+export default ({data}) => (
+
 	<Layout>
-		<StyledHero>
-			<Banner title='joseph kalogerakis' info='Painter and Digital Artist'>
-				<Link to='/gallery' className='btn-white'>portfolio</Link>
-			</Banner>
-		</StyledHero>
-	</Layout>
+	<StyledHero home='true' img={data.defaultBcg.childImageSharp.fluid}>
+		<Banner title='joseph kalogerakis' info='Painter and Digital Artist'>
+			<Link to='/gallery' className='btn-white'>portfolio</Link>
+		</Banner>
+	</StyledHero>
+	</Layout >
 )
+
+export const query = graphql`
+query {
+  defaultBcg:file(relativePath:{eq:"sifis3.jpg"}){
+    childImageSharp{
+      fluid(quality:90,maxWidth:4160){
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+}
+`

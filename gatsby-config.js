53049aba530e91af445d72e964dc97f403a866b1
magic8baller <lturis@gmail.com>
2019-09-10 14:11:53 +0300

    update config for siteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+	siteMetaData: {
+		title: 'SifisPort1',
+		description: "Explore joseph kalogerakis' artwork",
+		author: "@magic8baller",
+	},
 	plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }

c023a0716ba3694901e7b2cefc702a0169aea298
magic8baller <lturis@gmail.com>
2019-09-17 19:32:58 +0300

    about page button displays more text onclick

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,8 +2,8 @@ import Image from 'gatsby-image';
 import React from 'react';
 import styles from '../css/about.module.css';
 import {useAboutImage} from '../hooks';
+import AboutDetails from './AboutDetails';
 import Title from './StyledTitle';
-
 
 export default function About () {
 
@@ -25,9 +25,7 @@ export default function About () {
 					<h4>explore my world</h4>
 					<p>They made a good decision. Look at these words. Are they small words? And he referred to my words - if they're small, something else must be small. I guarantee you there's no problem, I guarantee. I don't think anybody knows it was Russia that wrote Lorem Ipsum, but I don't know, maybe it was. It could be Russia, but it could also be China. It could also be lots of other people. It also could be some wordsmith sitting on their bed that weights 400 pounds. Ok?
 		      </p>
-
-					<p>You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life. The best taco bowls are made in Trump Tower Grill. I love Hispanics! The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive.</p>
-					<button type='button' className='btn-primary'>read more</button>
+					<AboutDetails />
 				</article>
 			</div>
 		</section>
---
+++ src/components/AboutDetails.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const AboutDetails = () => {
+
+	const [showInfo, setInfo] = useState(false)
+
+	const toggleInfo = () => {
+		setInfo(showInfo => !showInfo)
+	}
+
+	const dummyMoreInfo = "You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life. The best taco bowls are made in Trump Tower Grill. I love Hispanics! The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive."
+	return (
+		<article>
+
+			<button type='button' className='btn-primary' onClick={toggleInfo}>read more</button>
+
+			{showInfo && (<p style={{paddingTop: "2rem"}}>{dummyMoreInfo}</p>)}
+		</article>
+	)
+}
+
+export default AboutDetails

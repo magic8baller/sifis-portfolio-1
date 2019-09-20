d1e75dcd6b7237d86dbc179a041c0b9fe171af3d
magic8baller <lturis@gmail.com>
2019-09-20 20:27:49 +0300

    toni text1

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from '../css/about.module.css';
-import {useAboutImage} from '../hooks';
 import img from '../images/itsame.jpg';
 import AboutDetails from './AboutDetails';
 import Title from './StyledTitle';
-import Image from 'gatsby-image'
+import {text} from '../aboutText'
+// import Image from 'gatsby-image'
 export default function About () {
 
-	const aboutImage = useAboutImage()
-	const mainImage = aboutImage[0].node.images[0].fluid
+	// const aboutImage = useAboutImage()
+	// const mainImage = aboutImage[0].node.images[0].fluid
 
 	// console.log(img)
 	return (
@@ -18,14 +18,18 @@ export default function About () {
 			<div className={styles.aboutCenter}>
 				<article className={styles.aboutImg}>
 					<div className={styles.imgContainer}>
-						<Image fluid={mainImage} alt='bad ass mofo' />
-						{/* <img src={img} alt='waa' /> */}
+						{/* <Image fluid={mainImage} alt='bad ass mofo' /> */}
+						<img src={img} alt='badassmofo' />
 					</div>
 				</article>
 				<article className={styles.aboutInfo}>
-					<h4>explore my world</h4>
-					<p>They made a good decision. Look at these words. Are they small words? And he referred to my words - if they're small, something else must be small. I guarantee you there's no problem, I guarantee. I don't think anybody knows it was Russia that wrote Lorem Ipsum, but I don't know, maybe it was. It could be Russia, but it could also be China. It could also be lots of other people. It also could be some wordsmith sitting on their bed that weights 400 pounds. Ok?
-		      </p>
+					<h4>meet the artist</h4>
+					<p>Describing an artist is a difficult task and I have to admit, that I would prefer if you, dear reader, would just look at the artwork itself and let <em>it</em> do the describing, not <em>me.</em></p>
+<p>
+Okay I get it, you are still here for the Information. But before I tell you the little that I know about the subject of your interest I have to confess I am not an author free of all sorts of bias, rather quite the opposite and perhaps a liar too.
+I once heard Joseph say "nothing is so common than the wish to be remarkable", err wait, no, Shakespeare said that; Joseph said something like, "when I make art, it comes from a dark place deep inside me and I can find no words to describe it".
+</p>
+
 					<AboutDetails />
 				</article>
 			</div>

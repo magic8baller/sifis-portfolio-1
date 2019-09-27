ff2db71acc3007f6d85a6d91710839af3c6aefd2
magic8baller <lturis@gmail.com>
2019-09-27 18:13:38 +0300

    change 404 image/banner

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,43 @@
+import {graphql} from "gatsby";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import React from "react";
 import Banner from '../components/Banner';
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import StyledHero from "../components/StyledHero";
 import styles from "../css/error.module.css";
-const Error = () => {
+// import img from '../images/oopsy.jpeg'
+
+
+const Error = ({data}) => {
 	return (
 		<Layout>
 			<SEO title="Error" />
+			<Banner title="Uh oh..." info="No page exists at this address">
+				<br/>
+			<StyledHero img={data.oopsy.childImageSharp.fluid}>
+			</StyledHero>
 			<header className={styles.error}>
-				<Banner title='oops youve hit a dead end'>
+				<br/><br/>
 					<AniLink fade to='/' className='btn-white'>
 						Return to Homepage
 				</AniLink>
-				</Banner>
 			</header>
+				</Banner>
+				<br/>
 		</Layout>
 	)
 }
 export default Error
+
+export const query = graphql`
+	query {
+		oopsy: file(relativePath: {eq: "oopsy.jpeg"}){
+			childImageSharp{
+				fluid(quality: 90, maxWidth: 4160){
+				...GatsbyImageSharpFluid_withWebp
+				}
+			}
+		}
+	}
+`

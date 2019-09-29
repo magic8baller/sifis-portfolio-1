ea3e7fd06d5dd9bd4cb03ec8dff60d1332981fa2
magic8baller <lturis@gmail.com>
2019-09-29 18:00:57 +0300

    change splash image to violent-violin

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default ({data}) => (
 
 export const query = graphql`
 query {
-  defaultBcg:file(relativePath:{eq:"sifis-self.jpg"}){
+  defaultBcg:file(relativePath:{eq:"violent-violin.jpg"}){
     childImageSharp{
       fluid(quality:90,maxWidth:4160){
         ...GatsbyImageSharpFluid_withWebp

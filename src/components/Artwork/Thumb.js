8a1392fcd00014033db28da1ff0d6b1623de1d1c
magic8baller <lturis@gmail.com>
2019-09-30 16:53:43 +0300

    contentful redirect gallery probs

--- src/components/Artwork/Thumb.js
+++ src/components/Artwork/Thumb.js
@@ -5,21 +5,21 @@ import styles from '../../css/art.module.css';
 
 const Thumb = ({art}) => {
 
-	let {collection, images} = art
+	let {collection, medium, images} = art
 	let mainImage = images[0].fluid;
 
-	// const thumbTitle = collection ? `${collection + ' collection'}` : `${medium + 's'}`
-	// const redirectLink = collection ? `${collection}` : `${medium + 's'}`
+	const thumbTitle = collection == 'abstract' ? `${collection + ' collection'}` : `${medium + 's'}`
+	const redirectLink = collection == 'abstract' ? `${collection}` : `${medium + 's'}`
 
 
 	return (
 		<article className={styles.art}>
 			<div className={styles.imgContainer}>
 				<Image fluid={mainImage} className={styles.img} alt='single Thumb' />
-				<AniLink fade className={styles.link} to={`/${collection}`}><span className={styles.thumbTitle}>{collection + 's'}</span></AniLink>
+				<AniLink fade className={styles.link} to={`/${redirectLink}`}><span className={styles.thumbTitle}>{thumbTitle}</span></AniLink>
 			</div>
 			<div className={styles.footer}>
-				<h2>{collection}</h2>
+				<h2>{thumbTitle}</h2>
 			</div>
 		</article>
 	)

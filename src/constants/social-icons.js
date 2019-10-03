1a8519e50c79736e710e234c4ae2cd662fc07328
magic8baller <lturis@gmail.com>
2019-10-03 09:41:24 -0400

    got rid of instagram link

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import {FaFacebook, FaInstagram, FaEnvelope} from 'react-icons/fa'
+import {FaEnvelope, FaFacebook} from 'react-icons/fa'
 export default [
 	{
 		icon: <FaFacebook />,
 		url: 'https://www.facebook.com/cerberusjoe'
 	},
+
 	{
-		icon: <FaInstagram />,
-		url: 'https://www.instagram.com/iosifkalogerakis/'
-	},
-	{icon: <FaEnvelope/>,
-		url: 'mailto:josagr2009@hotmail.com' }
+		icon: <FaEnvelope />,
+		url: 'mailto:josagr2009@hotmail.com'
+	}
 
 ]

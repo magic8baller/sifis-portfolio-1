a88a43f60bfcc76665a6c747fd7cc25031e90ae9
magic8baller <lturis@gmail.com>
2019-10-06 11:48:20 -0400

    add &nbsp; to contact label + icon

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -11,16 +11,16 @@ const Contact = () => {
 				<form action="https://formspree.io/josagr2009@hotmail.com" method="POST" className={styles.form}>
 					<div>
 						<input type="text" name="_gotcha" style={{display: 'none'}} />
-						<label htmlFor='name'>Name</label>
-						<FaRegUser /><input type="text" name='name' id='name' className={styles.formControl} />
+						<FaRegUser />&nbsp;<label htmlFor='name'>Name</label>
+						<input type="text" name='name' id='name' className={styles.formControl} />
 					</div>
 					<div>
-						<label htmlFor='email'>Email</label>
-						<FaEnvelopeOpenText /><input type="email" name='email' id='email' className={styles.formControl} />
+						<FaEnvelopeOpenText />&nbsp;<label htmlFor='email'>Email</label>
+						<input type="email" name='email' id='email' className={styles.formControl} />
 					</div>
 					<div>
-						<label htmlFor='message'>Message</label>
-						<FaUserEdit /><textarea name='message' id='message' rows='10' className={styles.formControl} />
+						<FaUserEdit />&nbsp;<label htmlFor='message'>Message</label>
+						<textarea name='message' id='message' rows='10' className={styles.formControl} />
 					</div>
 					<div>
 						<input type="submit" value='submit here' className={styles.submit} />
